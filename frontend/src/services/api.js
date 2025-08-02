1f30985edb8ec6a37b1c6819d6104794cad3dba5
leaky001 <lakybass19@gmail.com>
2025-08-02 16:24:47 +0100

    vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || " https://rentcheck-1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
